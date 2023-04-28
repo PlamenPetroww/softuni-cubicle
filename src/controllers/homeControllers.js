cdb6059c2bcc9e793d6057f9ca809e2912cce671
plamenpetroww <paco_montanamax@yahoo.com>
2023-04-28 10:58:32 +0200

    Add home controller

---
+++ src/controllers/homeControllers.js
@@ -0,0 +1,9 @@
+
+
+exports.getHomePat = (req, res) => {
+    res.render('index'); 
+};
+
+exports.getAboutPage = (req, res) => {
+    res.render('about');
+}
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,9 @@
-const cubeController = require('./controllers/cubeController');
 const router = require('express').Router();
+const cubeController = require('./controllers/cubeController');
+const homeController = require('./controllers/homeControllers');
 
-router.get('/', (req, res) => {
-    res.render('index'); 
- });
- 
- router.get('/about', (req, res) => {
-    res.render('about');
- });
- 
- router.get('./create', cubeController.getCreateCube);
+router.get('/', homeController.getHomePat);
+router.get('/about', homeController.getAboutPage);
+router.get('./create', cubeController.getCreateCube);
 
- module.exports = router;
+module.exports = router;

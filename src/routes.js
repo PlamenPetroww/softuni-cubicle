cb1ad29c05be4dc4f2c28a84dc860b82cf096e3c
plamenpetroww <paco_montanamax@yahoo.com>
2023-05-10 12:00:42 +0200

    Fix paths

--- src/routes.js
+++ src/routes.js
@@ -10,10 +10,10 @@ router.get('/404', homeController.getErrorPage);
 router.get('/cubes/:cubeId/attach', cubeController.getAttachAccessory);
 router.post('/cubes/:cubeId/attach', cubeController.postAttachAccessory);
 
-router.get('/create', cubeController.getCreateCube);
-router.post('/create', cubeController.postCreateCube);
+router.get('/cubes/create', cubeController.getCreateCube);
+router.post('/cubes/create', cubeController.postCreateCube);
 router.get('/cubes/:cubeId/details', cubeController.getDetails);
 
-router.use('/accessory', accessoryController);
+router.use('/accessories', accessoryController);
 
 module.exports = router;

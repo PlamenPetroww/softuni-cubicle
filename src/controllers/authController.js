6efa55dcfd15aa6a3a489c2cff943665305318d7
plamenpetroww <paco_montanamax@yahoo.com>
2023-05-15 13:25:16 +0200

    Send cookie to client

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -11,12 +11,12 @@ router.post('/login', async (req, res) => {
 
     try{
         const token = await authService.login(username, password);
-        console.log(token);
+        
+        res.cookie('auth', token, {httpOnly: true});
     }catch(err) {
         console.log(err);
-        return res.redirect('/');
     }
-    res.redirect('/');
+    return res.redirect('/');
 });
 
 router.get('/register', (req, res) => {

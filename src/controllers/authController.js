e34d6c6556267e5e33b6239a324e592eda5b933c
plamenpetroww <paco_montanamax@yahoo.com>
2023-05-16 08:43:06 +0200

    Add authorisation in one endpoint

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -11,7 +11,7 @@ router.post('/login', async (req, res) => {
 
     try{
         const token = await authService.login(username, password);
-        
+            
         res.cookie('auth', token, {httpOnly: true});
     }catch(err) {
         console.log(err);
--- src/lib/jsonwebtoken.js
+++ src/lib/jsonwebtoken.js
@@ -1,6 +1,14 @@
-const jwtCallback = require('jsonwebtoken');
 const util = require('util');
+const jwtCallback = require('jsonwebtoken');
 
+
+// jwtCallback.sign(payload, secret, options, (err, token) => {
+//     if(err) {
+//         return reject(err);
+//     }
+
+//     resolve(token);
+// });
 
 const jwt = {
     sign: util.promisify(jwtCallback.sign),

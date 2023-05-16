e5b79589eda15fb3a4a654a61999e511b30d96d1
plamenpetroww <paco_montanamax@yahoo.com>
2023-05-16 09:25:29 +0200

    Add jwt.token for user

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ const cubeController = require('./controllers/cubeController');
 const homeController = require('./controllers/homeControllers');
 const accessoryController = require('./controllers/accessoryController');
 const authController = require('./controllers/authController');
-
+const {isAuthenticated} = require('./middlewares/authMiddleware');
 
 router.get('/', homeController.getHomePat);
 router.get('/about', homeController.getAboutPage);
@@ -11,9 +11,9 @@ router.get('/404', homeController.getErrorPage);
 router.use('/', authController);
 
 router.get('/cubes/:cubeId/attach', cubeController.getAttachAccessory);
-router.post('/cubes/:cubeId/attach', cubeController.postAttachAccessory);
-router.get('/cubes/create', cubeController.getCreateCube);
-router.post('/cubes/create', cubeController.postCreateCube);
+router.post('/cubes/:cubeId/attach',cubeController.postAttachAccessory);
+router.get('/cubes/create', isAuthenticated, cubeController.getCreateCube);
+router.post('/cubes/create', isAuthenticated, cubeController.postCreateCube);
 router.get('/cubes/:cubeId/details', cubeController.getDetails);
 
 router.use('/accessories', accessoryController);
